55b86f515cf37aa929ebf529e3bf7ad37316b8ca
SMix <SMix123@users.noreply.github.com>
2017-05-05 12:57:10 -0300

    Corrigido erro grave.
    O eval não detectava o ID da pessoa que executava, o que seria um perigo.

--- MixyBot/comandos/eval.js
+++ MixyBot/comandos/eval.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 var moment = require("moment")
 const config = require("../config.json")
 exports.run = (bot, message, args) => {
+            if(message.author.id === config.dono) {
 if (message.content.includes('bot.token')) {
             message.channel.sendMessage('<:vpRedTick:257437215615877129> | RAIO PROTETOR! Protegi meu token de ser exibido!');
         } else {
@@ -11,4 +12,4 @@ if (message.content.includes('bot.token')) {
             } catch (err) {
                 message.channel.sendMessage(`:x: | ${err}`);
             }
-        }}
+        }}}
